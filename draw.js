1b5a9240c4019445ced79bbbd4ad33d4bb50f001
davidtwalsh <46041406+davidtwalsh@users.noreply.github.com>
2020-08-12 21:19:44 -0400

    Add files via upload

---
+++ draw.js
@@ -0,0 +1,37 @@
+const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+const scale = 10;
+
+const rows = canvas.height/scale;
+const columns = canvas.width/scale;
+
+var snake;
+
+(function setup(){
+	snake = new Snake();
+	fruit = new Fruit();
+
+	fruit.pickLocation();
+	console.log(fruit)
+
+	window.setInterval(() =>{
+		ctx.clearRect(0,0,canvas.width,canvas.height);
+		snake.update();
+		fruit.draw();
+		snake.draw();
+
+		if (snake.eat(fruit))
+		{
+			fruit.pickLocation();
+		}
+
+		snake.checkCollision();
+		document.querySelector(".score").innerText = snake.total;
+
+	},250);
+}());
+
+window.addEventListener('keydown',((evt) => {
+	const direction = evt.key.replace('Arrow','');
+	snake.changeDirection(direction);
+}))
